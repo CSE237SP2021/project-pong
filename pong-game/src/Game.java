108686d1fb6ff569b97c4f87b30b6a7bc8b4131e
nicolas-giordano <nicolas.giordano@wustl.edu>
2021-03-28 21:02:51 -0500

    fixed for something

--- pong-game/src/Game.java
+++ pong-game/src/Game.java
@@ -1,13 +1,33 @@
 import java.awt.*;
-
-import javax.swing.JPanel;
+import java.util.*;
+import javax.swing.*;
 
 public class Game extends JPanel implements Runnable{
 
     final int WIDTH = 700;
     final int HEIGHT = 500;
-
+    
     final Dimension DIMENSION = new Dimension(WIDTH, HEIGHT);
+    final int BALL_DIAMETER = 15;
+    final int PADDLE_WIDTH = 20;
+    final int PADDLE_HEIGHT = 75;
+
+    Thread thread;
+
+    Image image;
+
+    Graphics graphics;
+
+    Random rand;
+
+    Paddle player1;
+
+    Paddle player2;
+
+    Ball ball;
+
+	Score score;
+
 
     Game() {
 
