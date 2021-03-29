790fa03e30a4dec442a606e64cbc7c7142959599
zhangKevinH <31417589+zhangKevinH@users.noreply.github.com>
2021-03-28 19:35:20 -0700

    Added Ball Class

--- pong-game/src/Ball.java
+++ pong-game/src/Ball.java
@@ -1,3 +1,49 @@
-public class Ball {
+import java.util.*;
+import java.awt.*;
+
+
+public class Ball extends Rectangle{
     
+    int initV = 2; //Initial velocity
+	Random rand; //Random number
+	int Vx; //Initial X velocity
+	int Vy; //Initial Y velocity
+	
+	Ball(int x, int y, int width, int height){ //ball constructor
+		super(x,y,width,height); 
+		rand = new Random();
+		int randVx = random.nextInt(2);
+        int randVy = random.nextInt(2);
+
+		if(randVx == 0){
+            randVx--;
+        }
+		
+		if(randVy == 0){
+            randVy--;
+        }
+
+        setVx(randVx*initV);
+		setVy(randVy*initV);
+		
+	}
+	
+	public void draw(Graphics g) { //Draws ball
+        g.fillOval(x, y, height, width);
+		g.setColor(Color.white);
+	}
+
+    public void move() {
+		x += Vx;
+		y += Vy;
+	}
+
+	public void setVx(int randVx) { //Sets x velocity of ball
+		Vx = randVx;
+	}
+	public void setVy(int randVy) { //Sets y velocity of ball
+		Vy = randVy;
+	}
+
+
 }
